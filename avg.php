b5672f68f33c5cf48a2dcb3f4d84b48415657b4f
Tobiasz-Hurny <ordinarymammal@gmail.com>
2023-03-04 13:12:03 +0100

    srednia wazona

--- avg.php
+++ avg.php
@@ -1,6 +1,11 @@
 <?php
-	function srednia(a,b)
-	{
-		return (a+b)/2;
-	}
+$oceny = array(1,2,3,4,5);
+$wagi = array(5,4,3,2,1);
+$sumaWazona = 0;
+foreach( $oceny as $klucz => $ocena ){
+  $sumaWazona +=  $ocena * $wagi[$klucz];
+  $sumaWag += $wagi[$klucz];
+}
+ 
+$wynik = $sumaWazona / $sumaWag;
 ?>
